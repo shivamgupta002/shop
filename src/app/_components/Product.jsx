a0bae372f1865343fa61b2d6a67b90cd80aad8d8
shivamgupta02 <shivam.g@taltrip.com>
2024-09-29 21:00:36 +0530

    header done

---
+++ src/app/_components/Product.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const Product = () => {
+  return (
+    <>
+         <div className="container mt-40 mb-30">
+            <h3 className="text-center">Hover Effect Style : Demo - 21</h3>
+            <div className="row mt-30">
+                <div className="col-md-4 col-sm-6">
+                    <div className="box21">
+                        <img className="pic-1" src="https://www.w3schools.com/bootstrap4/img_avatar5.png"/>
+                        <div className="box-content">
+                            <h4 className="title">willimson</h4>
+                            <p className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mattis augue in odio suscipit, at.</p>
+                            <a className="read-more" href="#">read more</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-4 col-sm-6">
+                    <div className="box21">
+                        <img className="pic-1" src="https://www.w3schools.com/bootstrap4/img_avatar4.png"/>
+                        <div className="box-content">
+                            <h4 className="title">Kristiana</h4>
+                            <p className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mattis augue in odio suscipit, at.</p>
+                            <a className="read-more" href="#">read more</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-4 col-sm-6">
+                    <div className="box21">
+                        <img className="pic-1" src="https://www.w3schools.com/bootstrap4/img_avatar3.png"/>
+                        <div className="box-content">
+                            <h4 className="title">Kristiana</h4>
+                            <p className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mattis augue in odio suscipit, at.</p>
+                            <a className="read-more" href="#">read more</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Product
