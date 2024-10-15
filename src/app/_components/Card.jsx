49f4712c32dad6c18ae368bf9acc59533d1c077b
shivamgupta02 <shivam.g@taltrip.com>
2024-10-15 19:44:15 +0530

    add card design

--- src/app/_components/Card.jsx
+++ src/app/_components/Card.jsx
@@ -8,21 +8,21 @@ import CardActionArea from '@mui/material/CardActionArea';
 const CustomCard = () => {
   return (
    <>
-   <Card sx={{ maxWidth: 345 }}>
+   <Card sx={{ maxWidth: 345,margin:'1rem' }}>
       <CardActionArea>
         <CardMedia
           component="img"
           height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          alt="green iguana"
+          image=""
+          alt="img"
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
-            Lizard
+            Sewing machine
           </Typography>
           <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
+           We are providing various kind of sewing machine,fans,coller,etc
+             
           </Typography>
         </CardContent>
       </CardActionArea>
--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,51 @@
 "use client";
-import * as React from 'react';
+import * as React from "react";
 import Header from "./_components/Header";
-import AOS from 'aos';
-import 'aos/dist/aos.css'; 
-import CustomCard from './_components/Card';
+import AOS from "aos";
+import "aos/dist/aos.css";
+import CustomCard from "./_components/Card";
+import { Box, Typography } from "@mui/material";
 
 export default function Home() {
   React.useEffect(() => {
     AOS.init({
-      duration: 1000, 
+      duration: 1000,
       once: true,
     });
   }, []);
 
   return (
     <>
-      <Header/>
-      <CustomCard/>
+      <Header />
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          flexWrap: "wrap",
+          margin: "2rem",
+        }}
+      >
+        <CustomCard />
+        <CustomCard />
+        <CustomCard />
+        {/* <CustomCard />
+        <CustomCard />
+        <CustomCard /> */}
+
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          flexWrap: "wrap",
+          margin: "2rem",
+        }}
+      >
+        <Typography variant="h5">Sorry for in convience.</Typography>
+        <Typography variant="h3">We update aur product in some days </Typography>
+        </Box>
     </>
   );
 }
